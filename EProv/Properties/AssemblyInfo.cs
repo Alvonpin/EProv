d9a2c217bbff42cee3bc87559f67c18886e6d43a
Alvonpin <43116734+Alvonpin@users.noreply.github.com>
2020-01-29 13:48:34 +0100

    Eprov1

--- EProv/Properties/AssemblyInfo.cs
+++ EProv/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87435a23-681c-4c35-bcde-aea6389476d6")]
+[assembly: Guid("bbb237d8-8ef4-4530-b872-e6e77179dcf0")]
 
 // Version information for an assembly consists of the following four values:
 //
